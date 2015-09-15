d3cd1dde784536136f6d69df14a9ff6fb7c90266
Brian Boyko <brian.boyko@gmail.com>
2015-09-15 18:45:32 -0500

    frontend of criteria creator console logs correctly

--- client/app/presentations/presentations.controller.js
+++ client/app/presentations/presentations.controller.js
@@ -1,36 +1,57 @@
 angular.module('spkr.presentations', [])
-  .controller('PresentationsController', function($scope, $window, $location, Auth, Pres) {
-    
-    $scope.root = window.location.href.slice(0,window.location.href.lastIndexOf('/'));
-    
+  .controller('PresentationsController', function($scope, $window, $location,
+    Auth, Pres) {
+    //takes a raw string, seperates them by commas, and chucks them in an array of criteria. 
+    var makeCriteria = function(commaDelimitedList, isChecked) {
+      var criteria = ['organization', 'clarity', 'volume', 'posture',
+        'preparation', 'visual aids', 'connection', 'questions', 'overall'];
+      //If the user has defined custom criteria, parse that into an array of criteria. 
+      if (commaDelimitedList !== '' && isChecked) {
+        criteria = [];
+        criteria = commaDelimitedList.split(',')
+        for (var i = 0; i < criteria.length; i++)
+          while (criteria[i].indexOf(' ') === 0) {
+            criteria[i] = criteria[i].slice(1);
+          }
+      }
+      return criteria;
+    };
+
+    $scope.root = window.location.href.slice(0, window.location.href.lastIndexOf(
+      '/'));
+
     $scope.$watch(Auth.isAuth, function(authed) {
       if (authed) {
         $location.path('/presentations');
       } else {
-        $location.path('/')
+        $location.path('/');
       }
     }, true);
 
     var today = new Date().toISOString().split('T')[0];
     document.getElementsByName("date")[0].setAttribute('min', today);
 
-    $scope.submit = function(presentation){
-      Pres.createPresentation(presentation).then(function(data, err){
-        if(err) console.log(err);
-        $scope.feedbackUrl = $scope.root + "/feedback-form/" + data.newPresentation.presentationid;
+    $scope.submit = function(presentation) {
+
+      console.log('final', makeCriteria(presentation.customCriteriaList,
+        $scope.customizeChecked));
+      Pres.createPresentation(presentation).then(function(data, err) {
+        if (err) console.log(err);
+        $scope.feedbackUrl = $scope.root + "/feedback-form/" + data.newPresentation
+          .presentationid;
         $scope.getData();
         $scope.presentation = {};
       });
     };
 
-    $scope.getData = function(){
+    $scope.getData = function() {
       Auth.getAllData()
-        .then(function(data){
+        .then(function(data) {
           $scope.presentations = data.slice(1);
         })
-        .catch(function(error){
-          console.err(error)
+        .catch(function(error) {
+          console.err(error);
         });
     };
     $scope.getData();
-  })
+  });
